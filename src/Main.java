2ecedf33f7b2dce80f2cae6fa44d44e00a5195dd
guitaristishe <guitaristishe@gmail.com>
2023-05-18 19:56:51 +0300

    test commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Musteata Andrei");
+        String name = "Andrei";
+
+
+        String surname = "Musteata";
+        String jobTitle = "QA Engineer";
+        System.out.println("My name is " + name);
+        System.out.println("My surname is " + surname);
+        System.out.println("My job title is " + jobTitle);
+    }
+}
